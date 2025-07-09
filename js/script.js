5d2c0297fdba7f7b33fa0d0d480ab1233fa7167b
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-09 22:32:33 +0200

    click button js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 const button = document.getElementById(`button-light`)
+//img.getElementById(`lampadina-spenta`)
+
+button.addEventListener(`click`, function () {
+  //img.classList.remove(`lampadina-spenta`);
+  //lampadina - spenta.src = `img/yellow_lamp.png`;
+  //lampadina.alt = `lampadina-spenta`;
+  const lampadina = document.getElementById(`lampadina-spenta`)
+  lampadina.src = `img/yellow_lamp.png`;
+  lampadina.alt = `lampadina-spenta`;
+})
