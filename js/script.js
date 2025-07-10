cc80f665a668e6b028e0a94e17c618b93a501836
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-10 19:32:41 +0200

    correzione

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-const button = document.getElementById(`button-light`)
-//img.getElementById(`lampadina-spenta`)
+const lampadina = document.getElementById(`lampadina`)
+const button = document.getElementById(`button`)
+
+console.log(button)
 
 button.addEventListener(`click`, function () {
-  //img.classList.remove(`lampadina-spenta`);
-  //lampadina - spenta.src = `img/yellow_lamp.png`;
-  //lampadina.alt = `lampadina-spenta`;
-  const lampadina = document.getElementById(`lampadina-spenta`)
-  lampadina.src = `img/yellow_lamp.png`;
-  lampadina.alt = `lampadina-spenta`;
+  lampadina.src = `./img/yellow_lamp.png`;
 })
